93f99b87666be402837b22131fdaee68b2f78840
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-02-28 12:31:54 -0300

    somacde numeros

---
+++ soma.py
@@ -0,0 +1,8 @@
+print('Soma de dois numeros')
+print('Digite um numero')
+num1 = int(input())
+print('Digite outro numero')
+num2 = int(input())
+sum = num1 + num2
+print('A soma dos dois Numero é :')
+print( str(sum))
