db9477a7374127df1d279caa0aa42c90a7327ecd
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-02 19:20:51 -0300

    dobro/triplo

---
+++ Lista Aula02/02.py
@@ -0,0 +1,14 @@
+print('Digite primeiro numero')
+num1 = int(input())
+print('Digite segundo numero')
+num2 = int(input())
+
+def dobro(num1):
+    return num1 *2
+
+def triplo(num2):
+    return num2 *3
+
+print("o dobro do primeiro", dobro(num1))
+print("o triplo do segundo",triplo(num2))
+
