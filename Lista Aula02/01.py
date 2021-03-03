288d8b47d6bf8f1b7787affe47b2589d109f6d04
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-03 12:27:54 -0300

    operação

--- Lista Aula02/01.py
+++ Lista Aula02/01.py
@@ -1,3 +1,11 @@
+"""
+Converta as seguintes expressões matemáticas para que possam ser calculadas usando
+Python:
+10 + 20 x 30
+42 / 3
+(94 + 2) x 6 -1
+(94 + 2)4 + ( 10 / 1)
+"""
 print(10+20*30)
 print(4**2/3)
 print((9**4 +2)*(6-1))
