ae2333e773655cf350d7c32ff625e05e93b3a18a
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-03 12:52:02 -0300

    carro de aluguel

--- Lista Aula02/05.py
+++ Lista Aula02/05.py
@@ -2,7 +2,7 @@
 Escreva uma função que receba como argumento a quantidade de Km percorridos por um
 carro alugado pelo usuário, assim como a quantidade de dias pelos quais o carro foi alugado. A
 função deve retornar o preço a pagar, sabendo que o carro custa R$ 60,00 por dia e R$ 0,15
-por km rodado.
+por km rodado R$.
 """
 km_p = int(input("Digite a quantidade de km percorridos: "))
 dias = int(input("Digite a quantidade de dias: "))
