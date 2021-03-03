67fcc0f793249aed1b231f2ca6db304ba453e9be
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-03 20:40:51 -0300

    escolhar a operação

--- aula3/pratica01aula3.py
+++ aula3/pratica01aula3.py
@@ -3,17 +3,17 @@ num2=int(input("informe outro valor-> "))
 print("operação mul, div, sub, adi")
 op=str(input("informe a operação-> "))
 
-if op == adi:
-    return num1 + num2
+def resultado(num1, num2, op):
+    if op == "adi":
+        return num1 + num2
+    elif op == "sub":
+        return num1 - num2
+    elif op == "div":
+        return num1 / num2
+    elif op == "mul":
+        return num1 * num2
+    else:
+        return "invalido"
 
-elif op == sub:
-    return num1 - num2
-
-elif op == div:
-    return num1 / num2
-
-elif op == mul:
-    return num1 * num2
-
-else:
-    return ("invalido")
+x = resultado(num1, num2, op) 
+print(x)       
