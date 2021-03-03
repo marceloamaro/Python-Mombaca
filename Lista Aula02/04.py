aabb2cf59fbc5b6a8c71ac51828895d1dfcae795
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-03 12:49:28 -0300

    resto da divisao

--- Lista Aula02/04.py
+++ Lista Aula02/04.py
@@ -1,3 +1,9 @@
+"""
+Escreva uma função para calcular a redução do tempo de vida de um fumante. Pergunte a
+quantidade de cigarros fumados por dia e quantos anos ele já fumou. Considere que um
+fumante perde 10 minutos de vida a cada cigarro, a função deverá retornar quantos dias de vida
+um fumante perderá. Exiba o total em dias.
+"""
 qnt_Cigarros = int(input("Qnts cigarros por dia: "))
 anos_Fumando = int(input("Anos fumando: "))
 
