82fbee78b21aaf6fb6f04804420e4ceb6bcb3ee6
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-04 13:24:47 -0300

    impares

---
+++ Lista Aula03  Decisões e Repetições/09impares.py
@@ -0,0 +1,19 @@
+"""
+Gere as seguintes listas utilizando for e o metodo range():
+    • Uma lista de ímpares de 10 a 12o
+    • Uma lista pares de 2 a 1000
+    • Uma lista de multiplos de 2 em um intervalo de decrescente de 100 até 40
+    • Uma lista de primos de 44 até 99
+OBS: Pesquise pelo método append() na documentação
+"""
+
+impares = []
+for i in range(10, 120):
+    if i % 2 != 0:
+            impares.append(i)
+    print(" numeros impares", impares)
+    
+print("="*125)
+
+
+  
