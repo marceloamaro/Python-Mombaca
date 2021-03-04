7e537c6833a7f3c5df238b4a12ff3afc7f8d0292
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-04 10:16:59 -0300

    lancamento do foguete

---
+++ Lista Aula03  Decisões e Repetições/05.py
@@ -0,0 +1,10 @@
+"""
+aça um programa para escrever a contagem regressiva do lançamento de um foguete. O programa deve imprimir 10, 9, 8..., 1, 0 e "Fogo!", na tela
+"""
+print("contagem regressiva do lançamento do foguete")
+
+contagem = 10
+while contagem >= 0:
+    print(contagem)
+    contagem = contagem -1
+print("FOGO!!")
