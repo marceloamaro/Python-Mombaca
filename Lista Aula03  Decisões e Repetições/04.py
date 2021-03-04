309eec034f8080a1a428be26ec60e5235eee66fd
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-04 10:08:21 -0300

    emprestimo

---
+++ Lista Aula03  Decisões e Repetições/04.py
@@ -0,0 +1,19 @@
+"""
+Escreva uma função para aprovar o empréstimo bancário para compra de uma casa. Essa função deve receber o valor da casa a comprar, o salário e a quantidade de anos a pagar. O valor da prestação mensal não pode ser superior a 30% do salário. Calcule o valor da prestação como sendo o valor da casa a comprar dividido pelo número de meses a pagar.
+"""
+print('Calculo de emprestimo ')
+valor_casa = float(input('Valor da casa:'))
+salario = float(input('Salario:'))
+anos = int(input('Anos a pagar:'))
+meses = anos * 12
+parcelas = valor_casa / meses
+minimo = salario *(30/100)
+
+if parcelas < minimo:
+    print('seu emprestimo foi aprovado.')
+else:
+    print('seu emprestimo não foi aprovado,pois o valor das parcelas é maior que 30% do seu salário que é de',minimo)
+
+print('')
+print('O valor da casa dividido em', meses , 'meses é de:', parcelas,'por mes.')
+print('')
