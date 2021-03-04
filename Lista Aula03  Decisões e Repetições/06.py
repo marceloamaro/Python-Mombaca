de7a16e003aab748a923aa55c211ebcd90e8dd7b
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-04 10:42:02 -0300

    tabuada

---
+++ Lista Aula03  Decisões e Repetições/06.py
@@ -0,0 +1,13 @@
+"""
+Utilizando while imprima os resultados da tabuada de 2:
+A saída deve ser semelhante a:
+    • 2 x 1 = 2
+    • 2 x 2 = 4
+    """
+n1 = int (input('Deseja ver a tabuada de: '))
+
+cont = 0
+while (cont < 11):
+    tab = n1 * cont
+    print(n1, 'x', cont, '= ', tab)
+    cont += 1
