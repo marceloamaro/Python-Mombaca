95d040b1817a660f9fce0faa4f506377ae3b8c37
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-05 12:46:36 -0300

    cachaca

--- Lista Aula02/07.py
+++ Lista Aula02/07.py
@@ -3,27 +3,16 @@ Escreva uma função que receba a idade do usuário e indique se ele pode ou nã
 cara de cachaça
 """
 
-idade = int(input('Digite sua idade: '))
 
-if idade < 18:
-    print('Você é de Menor, vai tomar leite')
-elif idade >= 18 and idade < 45:
-    print('Você é de Maior, Pode encher a cara de caçacha')
-elif idade >= 45 and idade <= 80:
-    print('Cuidado com cirrose, Mas pode encher a cara de caçacha')
-else:
-    print('se tu tiver ainda vivo!, puder bebe')
-    """
-idade = int(input('Digite sua idade: '))
-
-def pinga(idade):
+def result(idade):
     if idade < 18:
-        return print('Você é de Menor, vai tomar leite')
+        print('Você é de Menor, vai tomar leite')
     elif idade >= 18 and idade < 45:
-        return print('Você é de Maior, Pode encher a cara de caçacha')   
+        print('Você é de Maior, Pode encher a cara de caçacha')
     elif idade >= 45 and idade <= 80:
-        return print('Cuidado com cirrose, Mas pode encher a cara de caçacha')
+        print('Cuidado com cirrose, Mas pode encher a cara de caçacha')
     else:
-        return print('se tu tiver ainda vivo!, puder bebe')
+        print('se tu tiver vivo!, puder bebe')
 
-   """   
+idade = int(input("qual sua idade?"))
+x = result(idade)
