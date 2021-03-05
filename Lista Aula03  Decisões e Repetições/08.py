4d8f0142f7dc49bf6e05992057283a7d582aa633
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-05 19:41:05 -0300

    maior da lista

--- Lista Aula03  Decisões e Repetições/08.py
+++ Lista Aula03  Decisões e Repetições/08.py
@@ -2,8 +2,10 @@
 Faça uma função que receba uma lista de números inteiros e retorne o maior elemento desta lista. Utilize o for
 """
 lista = []
-for i in range(0, 10):
-    lista.append(int(input(f"digite um valor para prosição {i}:")))
+def criar(lista):
+    for i in range(0, 10):
+        lista.append(int(input(f"digite um valor para prosição {i}:")))
 
-print(f"Voce digitou os valores da {lista}")
-print ("O maior elemento: ", max(lista))
+    print(f"Voce digitou os valores da {lista}")
+    print ("O maior elemento: ", max(lista))
+x = criar(lista)
