1f9c531c5f59cd1aae367223d2c3f75369aa1426
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-05 13:53:21 -0300

    equação do segundo grau X2 + 3x +3 = 0

--- Lista Aula02/13a.py
+++ Lista Aula02/13a.py
@@ -5,14 +5,16 @@ para calcular a raiz quadrada -> num ** 0.5
 
 def raiz(a, b, c):
     delta = (b**2 - 4*a*c)
+    print("o valor de delta é:", delta)
     if delta < 0:
         print("A equação não possui raizes reais.")
     elif delta == 0:
         raiz = (-1*b + (delta** 0.5))/(2 * a)
         print("A equacao possui apenas uma raiz que e ",raiz)
     elif delta > 0:
-        raiz1 =(-1*b + (delta** 0.5))/(2 * a)
-        raiz2 =(+1*b + (delta** 0.5))/(2 * a)
-        print("As raizes da equacao sao ",raiz1, raiz2)
+        x1 =(-1*b + (delta** 0.5))/(2 * a)
+        x2 =(+1*b + (delta** 0.5))/(2 * a)
+        print("O x1", x1)
+        print("O x2", x2)
     return
 raiz(1,3,3)
