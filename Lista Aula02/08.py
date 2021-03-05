ea05ba4232a313595c90c33a4b70ebfa264d69b2
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-05 13:05:18 -0300

    lei de morgan

--- Lista Aula02/08.py
+++ Lista Aula02/08.py
@@ -2,6 +2,16 @@
 P = True e Q = False. Aplique De Morgan na seguinte proposição e atribua o valor a uma
 variável - ~(p ^ (p v q)), essa variável deve ser retornada partir de uma função
 """
-p = True
-q = False
-print(not(p and(p or q)))
+
+def decisao(P,Q):
+    resposta = not P or(not P and not Q)
+    return resposta
+x = decisao(True,False) 
+print(x)
+
+
+def decisao(P,Q):
+    resposta = not(P and(P or Q))
+    return resposta
+x = decisao(True,False) 
+print(x)
