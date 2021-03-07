8d584e51d8123ba5956bf0e2a55ae607ec61d5ab
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-07 15:13:37 -0300

    primos

--- Lista Aula03  Decisões e Repetições/09primos.py
+++ Lista Aula03  Decisões e Repetições/09primos.py
@@ -16,6 +16,7 @@ for divisores in range(1, 10):
                 primos.append(i)
 print("numeros primos", primos)  
 """
+"""
 lista_numeros = [x for x in range(100)]
 
 lista_primos = list()
@@ -36,5 +37,18 @@ for number in lista_numeros:
 
     if soma_divisores == 2: 
         lista_primos.append(number)
+
+print(lista_primos)
+"""
+lista_primos = []
+
+for x in range(44,100):
+    cont=0
+
+    for y in range(1, x+1):
+        if x % y == 0:
+            cont += 1
+    if cont <= 2:
+            lista_primos.append(x)
 
 print(lista_primos)
