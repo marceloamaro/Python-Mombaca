ea51f4d10e76f8aca016af2a4c76725a1486c9f0
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-07 15:16:18 -0300

    primos

--- Lista Aula03  Decisões e Repetições/09primos.py
+++ Lista Aula03  Decisões e Repetições/09primos.py
@@ -6,40 +6,6 @@ Gere as seguintes listas utilizando for e o metodo range():
     • Uma lista de primos de 44 até 99
 OBS: Pesquise pelo método append() na documentação
 """
-"""
-primos = []
-divisores = 0
-for divisores in range(1, 10):
-    if primos % divisor == 0:
-        divisores = divisores + 1
-        if divisores > 1:
-                primos.append(i)
-print("numeros primos", primos)  
-"""
-"""
-lista_numeros = [x for x in range(100)]
-
-lista_primos = list()
-
-lista_divisores = [x for x in lista_numeros if x != 0]
-
-for number in lista_numeros: 
-
-    soma_divisores = 0 
-
-    for divisor in lista_divisores:
-
-        if number % divisor == 0: 
-            soma_divisores += 1
-        
-        elif number < divisor:
-            break
-
-    if soma_divisores == 2: 
-        lista_primos.append(number)
-
-print(lista_primos)
-"""
 lista_primos = []
 
 for x in range(44,100):
