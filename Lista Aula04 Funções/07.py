8164160393b8b27b52ea01624e5683c2b1d6644f
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-14 12:56:13 -0300

    cifra cezar

---
+++ Lista Aula04 Funções/07.py
@@ -0,0 +1,54 @@
+"""Pesquise sobre a Cifra de Cézar e crie duas funções, uma para crifrar uma mensagem e outra para decifrar. Utilize como parâmetro a função da questão 6. """
+
+print("Escolha entre ('criptografar', 'c') ou ('descriptografar', 'd')")
+opcao = input("Deseja criptografar ou descriptografar? ")
+opcao = opcao.lower()
+print("Digite o valor da chave entre 1 e 26")
+chave = int(input("Digite o valor da chave: "))
+mensagem = input("Digite a mensagem: ")
+   
+def encripta(mensagem, chave):
+    cripto = ''
+    for i in mensagem:
+        if 'A' <= i <= 'Z':
+            if ord(i) + chave > ord('Z'):
+                cripto += chr((ord('A') + chave - (ord('Z') + 1 - ord(i))))
+            else:
+                cripto += chr(ord(i) + chave)
+        elif 'a' <= i <= 'z':
+            if ord(i) + chave > ord('z'):
+                cripto += chr((ord('a') + chave - (ord('z') + 1 - ord(i))))
+            else:
+                cripto += chr(ord(i) + chave)
+        else:
+            cripto += i   
+                 
+    return cripto
+
+def descripta(mensagem, chave):
+    cripto = ''
+    for i in mensagem:
+        if 'A' <= i <= 'Z':
+            if ord(i) - chave < ord('A'):
+                cripto += chr(ord('Z') - (chave - (ord(i) + 1 - ord('A'))))
+            else:
+                cripto += chr(ord(i) - chave)
+        elif 'a' <= i <= 'z':
+            if ord(i) - chave < ord('a'):
+                cripto += chr(ord('z') - (chave - (ord(i) + 1 - ord('a'))))
+            else:
+                cripto += chr(ord(i) - chave)
+        else:
+            cripto += i
+    return cripto
+
+if opcao == 'c' or opcao == 'criptografar':
+    encripta(mensagem, chave)
+    print(encripta(mensagem, chave))
+
+elif opcao == 'd' or opcao == 'descriptografar':
+    descripta(mensagem, chave)
+    print(descripta(mensagem, chave))
+
+    
+
