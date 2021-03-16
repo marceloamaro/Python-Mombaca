7bbd16915d54017a7b0a4274e78ab5ddd0b09232
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-16 09:05:53 -0300

    lista

---
+++ Lista Aula06 - Listas e Tuplas/01.py
@@ -0,0 +1,15 @@
+"""Faça uma função que receba duas listas e que gere uma terceira com os elementos das duas primeiras. """
+"""
+lista1 = [1, 2, 3, 4]
+lista2 = [5, 6, 7, 8]
+
+
+soma = list(map(lambda x, y: (x + y), lista1, lista2))
+
+print(soma)   
+"""
+lista1 = [1, 2, 3, 4]
+lista2 = [5, 6, 7]
+for i in range(len(lista2)):
+    lista1.append(lista2[i])
+print(lista1) 
