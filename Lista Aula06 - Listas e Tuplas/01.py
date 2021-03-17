2e8248425e1cdec7f8e7e3adf6ce96ad13c5c2ff
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-17 08:20:51 -0300

    lista

--- Lista Aula06 - Listas e Tuplas/01.py
+++ Lista Aula06 - Listas e Tuplas/01.py
@@ -7,9 +7,21 @@ lista2 = [5, 6, 7, 8]
 soma = list(map(lambda x, y: (x + y), lista1, lista2))
 
 print(soma)   
-"""
+
 lista1 = [1, 2, 3, 4]
 lista2 = [5, 6, 7]
 for i in range(len(lista2)):
     lista1.append(lista2[i])
 print(lista1) 
+"""
+lista1 = [1, 2, 3, 4]
+lista2 = [5, 6, 7, 8]
+
+
+def lista_final(lista1,lista2):
+    lista3 = []
+    lista3.extend(lista1)
+    lista3.extend(lista2)
+    print(f"{lista3}")
+
+lista_final(lista1,lista2)
