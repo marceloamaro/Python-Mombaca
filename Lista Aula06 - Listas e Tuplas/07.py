16c1432c1cb44f82120ca9ba6cdeadb0c7afd4bb
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-21 18:26:30 -0300

    uniao da lista

--- Lista Aula06 - Listas e Tuplas/07.py
+++ Lista Aula06 - Listas e Tuplas/07.py
@@ -1,8 +1,8 @@
 """Escreva uma função que receba uma lista de vários inteiros e retorne um único inteiro composto por todos os itens da lista informada."""
 
-s = '0,1,2,3,4,5,6,7,8,9,10,11,15,100,111'
-digits = []
-for symbol in s:
-    if '12345'.find(symbol) != -1:
-        digits.append(int(symbol))
-print(digits)
+a=[1,2,3,4]
+def f(l):
+    if not l: return 0
+    return l[-1] + f(l[:-1]) * 10
+
+print(f(a))
