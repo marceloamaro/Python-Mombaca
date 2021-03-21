0e7b3c794dd13014847f2b644a11a6c5d8c08f0d
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-21 18:24:40 -0300

    dic conprehension e list comprehension

--- Lista Aula07 - Dicionários e Sets/04.py
+++ Lista Aula07 - Dicionários e Sets/04.py
@@ -2,7 +2,8 @@
 
 nome="marcelo"
 
-def estrutura(nome):
-    dic = {x : [x for x in range(7) ] for x in nome}   
-    print(dic) 
-estrutura(nome)    
+def dicionario(nome):
+    dic = {x : [x for x in range(10) if x %2 == 0 ] for x in nome}
+    print(dic)
+
+dicionario(nome)    
