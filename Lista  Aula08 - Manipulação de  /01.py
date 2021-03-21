bca6e37da91d172ef14aae1a07f17a468756367e
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-21 18:55:14 -0300

    dados.txt numeros

--- Lista  Aula08 - Manipulação de  /01.py
+++ Lista  Aula08 - Manipulação de  /01.py
@@ -32,6 +32,24 @@ with open("impares_dois.txt", "w") as impares2:
             if int(l) >= 10 and int(l) <= 800 : 
                 if int(l) % 2 != 0:
                     impares2.write(l)    
-"""            
+            
 
 #Gere um arquivo somente com números primos
+with open("primos.txt", "w") as primos2:
+    with open("numero.txt") as linha:
+        for l in linha.readlines(): 
+            if int(l) % 2==1:
+                primos2.write(l)
+"""
+#Gere 3 arquivos, sendo (divisores de 2, 3 e 5)
+with open("divisores2.txt", "w") as divisores2:
+    with open("divisores3.txt", "w") as divisores3:
+        with open("divisores5.txt", "w") as divisores5:
+            with open("numero.txt") as linha:
+                for l in linha.readlines(): 
+                    if int(l) % 2==0:
+                        divisores2.write(l)
+                    if int(l) % 3==0:
+                        divisores3.write(l)  
+                    if int(l) % 5==0:
+                        divisores5.write(l)  
