6a829bde7bbcd082344c4569a94872087f8df886
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-03-23 10:34:28 -0300

    opcao do set

--- Lista Aula07 - Dicionários e Sets/07.py
+++ Lista Aula07 - Dicionários e Sets/07.py
@@ -1,28 +1,25 @@
 """Faça uma função que retorne uma das três operações básicas entre sets: *A função deve receber dois conjuntos que terão intens adicionados manualmente pelo usuário *A operação que será retornada também será escolhida pelo usuário"""
-lista = []
+lista1 = []
 lista2 = []
 
-entrada1 = input("\nDigite valores para lista 1:\n ")
-lista.append(entrada1)
-#print(lista)
-set1 = set(lista)
-entrada2 = input("\nDigite valores para lista 2: \n")
-lista2.append(entrada2)
-#print(lista2)
+lista1 = [(input("Digite os 5 itens da lista 1\n")) for c in range(5)]
+lista2 = [(input("Digite os 5 itens da lista 2\n")) for c in range(5)]
+
+set1 = set(lista1)
 set2 = set(lista2)
+
 print("d = Diferença , u = União ou i = Intesecção")
-op = str(input("digite a operação do set-->"))
+op = str(input("digite a operação do set-->\n"))
+
 def opcao(set1, set2, op):
     if op == "d":
-        return (set1  - set2)
+        print(set1  - set2)
     elif op == "u":
-        return (set1  | set2)
+        print(set1  | set2)
     elif op == "i":
-        return (set1.intersection(set2))
+        print(set1.intersection(set2))
     else:
         return "invalido, informe d = Diferença , u = União ou i = Intesecção"
 
-x = opcao(set1, set2, op)
-print(x)
-
+opcao(set1, set2, op)
     
